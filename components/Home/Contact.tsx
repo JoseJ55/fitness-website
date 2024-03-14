e0b2e7f4281f3ed77ff5e444d5c644274eb9239d
JoseJ55 <josejimenez826@gmail.com>
2024-03-13 23:22:07 -0700

    added animation to contact form

--- components/Home/Contact.tsx
+++ components/Home/Contact.tsx
@@ -1,35 +1,88 @@
+"use client";
+
+import React, { useState, useEffect, useRef } from 'react';
+
+import Image from "next/image";
+
 function Contact() {
-  return (
-    <div className='flex justify-center items-center p-32 bg-custom-background w-full'>
-        <div className='w-4/12 flex flex-col justify-start items-start gap-7'>
-            <div className='w-full flex flex-col gap-2'>
-                <p className='text-custom-main text-xl'>
-                    Stay in touch
-                </p>
+    const [submitted, setSubmitted] = useState(false);
 
-                <p className='text-custom-main'>
-                    Join our a new letter to stay in touch with our activity.
-                </p>
-            </div>
+    const handleSubmit = () => {
+        setSubmitted(true);
+    };
 
-            <div className='w-full flex flex-col gap-2 justify-start items-start'>
-                <input 
-                    className='w-full p-3'
-                    type='text' 
-                    placeholder='Email' />
+    return (
+        <div className='flex justify-center items-center p-32 bg-custom-background w-full'>
+            <div 
+                className={`
+                    w-4/12 
+                    flex 
+                    flex-col 
+                    justify-start 
+                    items-center 
+                    gap-2 
+                    transition-all
+                    duration-1000
+                    ease-in-out
+                    ${submitted ? 'opacity-100 translate-x-1/2' : 'opacity-0 -translate-x-1/2'}
+                `}
+            >
+                <p className='text-custom-main text-xl'>Thank you for staying in touch with us</p>
 
-                <input 
-                    className='
-                        bg-custom-main
-                        px-2
-                        py-1
-                        text-sm
-                    '
-                    type='submit' />
+                <div className='w-full flex justify-center items-center'>
+                    <Image
+                        width={450}
+                        height={450}
+                        // layout='fill'
+                        // objectFit='contain'
+                        src='/assets/claudio-schwarz-vj3nOtiX85Y-unsplash.jpg'
+                        alt='community message' />
+                </div>
+            </div> 
+
+            <div 
+                className={`
+                    w-4/12 
+                    flex 
+                    flex-col 
+                    justify-start 
+                    items-start 
+                    gap-7 
+                    transition-all
+                    duration-1000
+                    ease-in-out
+                    ${submitted ? 'opacity-0 translate-x-1/2' : 'opacity-100 -translate-x-1/2'}
+                `}
+            >
+                <div className='w-full flex flex-col gap-2'>
+                    <p className='text-custom-main text-xl'>
+                        Stay in touch
+                    </p>
+
+                    <p className='text-custom-main'>
+                        Join our news letter to stay in touch with our activity.
+                    </p>
+                </div>
+
+                <div className='w-full flex flex-col gap-2 justify-start items-start'>
+                    <input 
+                        className='w-full p-3'
+                        type='text' 
+                        placeholder='Email' />
+
+                    <input 
+                        className='
+                            bg-custom-main
+                            px-2
+                            py-1
+                            text-sm
+                        '
+                        onClick={handleSubmit}
+                        type='submit' />
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Contact
